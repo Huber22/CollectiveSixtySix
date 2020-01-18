77f7140eaa3c0096319f56baaeeeb1f3f9b9d81c
Huber22 <nickhuber22@gmail.com>
2020-01-18 11:53:38 -0600

    menu

---
+++ CollectiveSixtySix/Assets/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public bool Level1Complete;//start gamestate 2
+    public bool Level2Complete;//start gamestate 3
+    public bool Level5Complete;//start gamestate 4 maybe
+
+    private static GameManager instance = null;
+    public static GameManager Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if ((instance != null) && (instance != this))
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Level1Complete = false;
+        Level2Complete = false;
+        Level5Complete = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CollectiveSixtySix/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void acheivements()
+    {
+        Debug.Log("Acheivements menu");
+    }
+    public void Exit()
+    {
+        Debug.Log("ExitGame");
+    }
+    public void Play()
+    {
+        Debug.Log("Play level 1");
+    }
+    public void LevelSelect()
+    {
+        Debug.Log("Level select menu");
+    }
+    public void Options()
+    {
+        Debug.Log("options menu");
+    }
+    public void Credits()
+    {
+        Debug.Log("Play Credits");
+    }
+}
